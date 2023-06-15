3e59d868fd9dfbb6e0dcb69634b9598417187cd4
bonfas oluoch <79736339+Bonfas-68@users.noreply.github.com>
2023-06-15 10:19:28 +0300

    modify the sidebar buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,43 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='b__sidebar'>
-      <div className="b__side-button"><button>Home</button></div>
-      <div className="b__side-button"><button>Profile</button></div>
-      <div className="b__side-button"><button>Followers</button></div>
-      <div className="b__side-button"><button>Votes</button></div>
-      <div className="b__side-button"><button>Collections</button></div>
-      <div className="b__side-button"><button>Logout</button></div>
-      <div className="b__side-button"><button>Share a method</button> </div>
+      <div className="b__side-button">
+        <button className='active-btn'>
+        <span>🏠</span>
+        <div className="b__side-button--text">Home</div>
+        </button>
+      </div>
+      <div className="b__side-button">
+        <button>
+        <span>👤</span>
+        <div className="b__side-button--text">Profile</div>
+        </button>
+      </div>
+      <div className="b__side-button">
+        <button id='top'>
+        <span>👨‍👧‍👧</span>
+        <div className="b__side-button--text">Followers</div>
+        <span className="top__num">12</span>
+        </button>
+      </div>
+      <div className="b__side-button">
+        <button id='top'>
+        <span>❎</span>
+        <div className="b__side-button--text">Votes</div>
+        <span className="top__num">22</span>
+        </button>
+      </div>
+      <div className="b__side-button">
+        <button id='top'>
+        <span>🏫</span>
+        <div className="b__side-button--text">Collections</div>
+        <span className="top__num">2</span>
+        </button>
+      </div>
+      <div className="b__side-button"><button>
+        <span></span>Logout</button></div>
+      <div className="b__side-button"><button>
+        <span></span>Share a method</button> </div>
     </div>
   )
 }

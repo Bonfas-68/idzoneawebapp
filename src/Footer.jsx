ccb3df341dc9c4c42ed01514f94a129fdc44a5a7
bonfas oluoch <79736339+Bonfas-68@users.noreply.github.com>
2023-06-15 10:25:14 +0300

    integrate footer to our category section

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='app__footer'>
+        <p>@idzonea web app &copy; 2023 </p>
+        <a href="#">policy | regulations</a>
+    </div>
+  )
+}
+
+export default Footer
